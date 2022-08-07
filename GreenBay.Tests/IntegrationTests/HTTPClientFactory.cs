66b87037cd3923c30afc6e1f7679b26d13627193
FilipKarlubik <filip.karlubik@gmail.com>
2022-08-07 13:34:57 +0200

    add 2 integr pests

--- GreenBay.Tests/IntegrationTests/HTTPClientFactory.cs
+++ GreenBay.Tests/IntegrationTests/HTTPClientFactory.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Linq;
 using System.Net.Http;
 
 namespace GreenBay.Tests.IntegrationTests
@@ -35,15 +36,21 @@ namespace GreenBay.Tests.IntegrationTests
                         var appDb = scopedServices.GetRequiredService<ApplicationContext>();
                         appDb.Database.EnsureDeleted();
                         appDb.Database.EnsureCreated();
-                        appDb.Users.AddRange(Constants.Users);
-                        appDb.SaveChanges();
-                        appDb.Items.AddRange(Constants.Items);
-                        appDb.SaveChanges();
+                        if (appDb.Users.Count() == 0)
+                        {
+                            appDb.Users.AddRange(Constants.Users);
+                            appDb.SaveChanges();
+                        }
+                        if (appDb.Items.Count() == 0)
+                        {
+                            appDb.Items.AddRange(Constants.Items);
+                            appDb.SaveChanges();
+                        }
                     }
                 });
             });
             _client = appFactory.CreateClient();
-            var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJGaWxpcCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImZpbGlwLmtybHViaWtAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE2NTk4MDExMDgsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzExLyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzExLyJ9.wfDUN7ARNEEV0CQ4XGBBKlvyr2IMmjMWAhjCLeO_YIs";
+            var accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJGaWxpcCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImZpbGlwLmtybHViaWtAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE5NzU0MjU4ODMsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzExLyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQ0MzExLyJ9.0nNQ6UXV_KHvidPkgr8yvat4BDVKm3ex3JxAdjO_i1Q";
             _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
         }
     }

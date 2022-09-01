1acb95c3a3083f413dde72e5aa05d231bc5b3bd9
FilipKarlubik <filip.karlubik@gmail.com>
2022-09-01 19:36:46 +0200

    move to dotnet6

--- GreenBay/Controllers/HomeController.cs
+++ GreenBay/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GreenBay.Context;
+using GreenBay.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace GreenBay.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationContext _db;
+
+        public HomeController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
         [Route("/hello")]
         public IActionResult Hello()
         {
-            return View();
+            User user = _db.Users.FirstOrDefault();
+            return View(user);
         }
     }
 }
